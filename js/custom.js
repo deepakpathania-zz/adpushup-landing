73b44bab7bc41c7208f5fbe8d79256a73f9b7837
deepakpathania <deepakpathania789@gmail.com>
2017-08-13 22:44:29 +0530

    initial layout

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+// preloader
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+$(function() {
+    new WOW().init();
+    $('.templatemo-nav').singlePageNav({
+    	offset: 70
+    });
+
+    /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    // $('.navbar-collapse a').click(function(){
+    //     $(".navbar-collapse").collapse('hide');
+    // });
+})
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(50);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(50);
+});
+
+
+$(window).resize(function() {
+    $('#home').height($(window).height()-70);
+}).resize();
